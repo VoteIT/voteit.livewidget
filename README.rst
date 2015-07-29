1bc9337902a5781fd945a5a66e5f4af48e13d0b0
robinharms <robin@betahaus.net>
2015-07-29 15:33:01 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,6 @@
+VoteIT Live widget
+==================
+
+Displays status and latest entries from a meeting.
+
+Note: This will soon be migrated to the Arche version of VoteIT, when the current sites have been migrated over.
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+
+requires = (
+    'pyramid',
+    'voteit.core',
+    )
+
+setup(name='voteit.livewidget',
+      version='0.1dev',
+      description='',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='VoteIT development team + contributors',
+      author_email='info@voteit.se',
+      url='http://www.voteit.se',
+      keywords='web pylons pyramid voteit',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = requires,
+      tests_require= requires,
+      test_suite="voteit.livewidget",
+      entry_points = """\
+      """,
+      )
