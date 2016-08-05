fdbb307ccfd4c79a6e6c47af08bd722c9269d5ad
Robin Harms Oredsson <robin@betahaus.net>
2016-08-05 11:34:40 +0200

    Update __init__.py
    Must be a property, accessing the old storage

--- voteit/livewidget/__init__.py
+++ voteit/livewidget/__init__.py
@@ -7,3 +7,12 @@ _ = TranslationStringFactory('voteit.livewidget')
 def includeme(config):
     config.include('.views')
     config.include('.schemas')
+
+    #Make sure meeting has a 'live_widget_enabled' property, accessing the old storage
+    from voteit.core.models.meeting import Meeting
+    
+    def _lw_getter(self):
+        return self.get_field_value('live_widget_enabled', False)
+    def _lw_setter(self, value):
+        self.set_field_value('live_widget_enabled', value)
+    Meeting.live_widget_enabled = property(_lw_getter, _lw_setter)
